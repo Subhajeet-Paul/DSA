7f8e40cec6158036279161ca75ca10a59064b320
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2025-03-16 02:03:44 +0530

    remove all occurence solution

---
+++ remove_all_occurrences.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "aabasabcsabc";
+    string part = "abc";
+
+    while(str.length()>0 && str.find(part) < str.length()){
+        str.erase(str.find(part), part.length());
+    }
+
+    cout << str << endl;
+    return 0;
+}
