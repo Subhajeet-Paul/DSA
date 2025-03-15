cd0e8c4e4597d38966c511480e38a321778c4c98
Subhajeet-Paul <subhajeetpaul28@gmail.com>
2025-03-16 01:57:17 +0530

    valid palindrome modified code

--- valid_palindrome.cpp
+++ valid_palindrome.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 bool isAlphaNumerc(char c){
     if((c>='a' && c<='z')||(c>='A' && c<='Z')||(c>='0' && c<='9')){
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
 
 bool isPalindrome(string s) {
-    int st = 0 , end = s.length()-1;
+    int st = 0;
+    int end = s.length()-1;
     while(st<end){
         if(!isAlphaNumerc(s[st])){
             st++;
@@ -22,17 +23,15 @@ bool isPalindrome(string s) {
             continue;
         }
         if(tolower(s[st])!=tolower(s[end])){
-            return false;
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
 int main(){
     string s = "mom";
     
-    bool result = isPalindrome(s);
-
-    if(result){
+    if(isPalindrome(s)){
         cout<<"Palindrome"<<endl;
     }else{
         cout<<"Not Palindrome"<<endl;
